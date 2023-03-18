799abb826a12c920426dbc242d0203312ea9b308
kiko <cahenriquesoares@gmail.com>
2023-03-17 22:05:19 -0300

    Inserido o default do DEBUG no decouple como False

--- pypro/settings.py
+++ pypro/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
