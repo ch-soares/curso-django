a9f4432e21f4e3d2e0d4e1dcf3a42fb78c59560f
kiko <cahenriquesoares@gmail.com>
2023-03-17 22:10:58 -0300

    Colocado valores default em ALLOWED_HOSTS e SECRET_KEY para tentar realizar deploy no render

--- pypro/settings.py
+++ pypro/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='CHAVE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='localhost')
 
 
 # Application definition
