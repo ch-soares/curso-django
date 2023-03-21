ed33e3d9b520bb0172c3b6d0f5ac429eaff21337
kiko <cahenriquesoares@gmail.com>
2023-03-21 11:33:01 -0300

    Configurado a lib Collectstatic nas configurações do AWS

--- pypro/settings.py
+++ pypro/settings.py
@@ -157,10 +157,11 @@ if AWS_ACCESS_KEY_ID:
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
     COLLECTFAST_ENABLED = True
+    COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
     # Static Assets
     # ---------------------------------------------------
-    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'  # Não está como na vídeo-aula. Ver link: https://github.com/pythonprobr/pythonpro-website/blob/master/pythonpro/settings.py#L259
     STATIC_S3_PATH = 'static'
     STATIC_ROOT = f'/{STATIC_S3_PATH}/'
     STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
