6716a6ed21b697c4f0b1b9552f550afc93e8edd0
kiko <cahenriquesoares@gmail.com>
2023-04-05 23:48:18 -0300

    Integra aplicação com o Sentry

--- pypro/settings.py
+++ pypro/settings.py
@@ -173,3 +173,11 @@ if AWS_ACCESS_KEY_ID:
 
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
+
+# Configuração do Sentry
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()])
