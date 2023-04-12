166a12495294ec7132aec21abd2230c0ae330fdb
kiko <cahenriquesoares@gmail.com>
2023-04-12 17:09:37 -0300

    Inseridos contexto nos templates
    close #63

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 
+videos = [
+        {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 816305935},
+        {'slug': 'instalacao-windows', 'titulo': 'Instalação do Windows', 'vimeo_id': 817023152},
+    ]
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = videos_dct[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
