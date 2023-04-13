a654fe0e59b7435d87574c2eba89d0b2d5abca7e
kiko <cahenriquesoares@gmail.com>
2023-04-13 12:00:20 -0300

    Finalizado composição de templates
    close #69

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse
+
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+    def get_absolute_url(self):
+        return reverse('aperitivos:video', args=(self.slug,))
 
 
 videos = [
-        {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 816305935},
-        {'slug': 'instalacao-windows', 'titulo': 'Instalação do Windows', 'vimeo_id': 817023152},
+    Video('motivacao', 'Video Aperitivo: Motivação', '816305935'),
+    Video('instalacao-windows', 'Instalação do Windows', '817023152'),
     ]
-videos_dct = {dct['slug']: dct for dct in videos}
+
+videos_dct = {dct.slug: dct for dct in videos}
 
 
 def video(request, slug):
--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -19,3 +19,7 @@ def test_title(resp):
 
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
+
+
+def test_email_link(resp):
+    assert_contains(resp, 'href="ramalho@python.com.br"')
