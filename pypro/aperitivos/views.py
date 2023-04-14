4e36646d68be58ac4eca32c8fc441565913081fe
kiko <cahenriquesoares@gmail.com>
2023-04-14 14:18:56 -0300

    Criado modelo e os primeiros testes de bd
    close #71

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,27 +1,18 @@
-from django.shortcuts import render
-from django.urls import reverse
-
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
+from django.shortcuts import render, get_object_or_404
+from pypro.aperitivos.models import Video
 
 
 videos = [
-    Video('motivacao', 'Video Aperitivo: Motivação', '816305935'),
-    Video('instalacao-windows', 'Instalação do Windows', '817023152'),
+    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id='816305935'),
+    Video(slug='instalacao-windows', titulo='Instalação do Windows', vimeo_id='817023152'),
     ]
+
 
 videos_dct = {dct.slug: dct for dct in videos}
 
 
 def video(request, slug):
-    video = videos_dct[slug]
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
 
 
