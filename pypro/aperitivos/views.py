4a476e59218eb411ad7b6f525cf316ca15100a56
kiko <cahenriquesoares@gmail.com>
2023-04-14 16:50:05 -0300

    Criado testes de bd
    close#73

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,19 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from pypro.aperitivos.models import Video
 
 
-videos = [
-    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id='816305935'),
-    Video(slug='instalacao-windows', titulo='Instalação do Windows', vimeo_id='817023152'),
-    ]
-
-
-videos_dct = {dct.slug: dct for dct in videos}
-
-
 def video(request, slug):
     video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
 
 
 def indice(request):
+    videos = Video.objects.order_by('criado_em').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
