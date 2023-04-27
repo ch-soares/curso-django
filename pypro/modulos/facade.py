893d140aaf3fdf37f05bc8a199f3f346f5f239d8
kiko <cahenriquesoares@gmail.com>
2023-04-27 10:46:22 -0300

    Cria relacionamento entre módulos e aulas

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 
 
 def listar_modulos_ordenados() -> List[Modulo]:
@@ -13,3 +13,11 @@ def listar_modulos_ordenados() -> List[Modulo]:
 
 def encontrar_modulo(slug: str) -> Modulo:
     return Modulo.objects.get(slug=slug)
+
+
+def lista_aulas_de_modulos_ordenadas(modulo: Modulo):
+    return modulo.aula_set.order_by('order').all()
+
+
+def encontrar_aula(slug: str) -> Aula:
+    return Aula.objects.get(slug=slug)
