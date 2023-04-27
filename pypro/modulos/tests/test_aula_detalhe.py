028ef16471339bec9d6bb1f86f15ed1ca8034330
kiko <cahenriquesoares@gmail.com>
2023-04-27 16:45:18 -0300

    Implementa funcionalidade Breadcrumb com Bootstrap

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -27,3 +27,7 @@ def test_titulo(resp, aula: Aula):
 
 def test_vimeo(resp, aula: Aula):
     assert_contains(resp, f'<iframe src="https://player.vimeo.com/video/{aula.vimeo_id}?')
+
+
+def test_modulo_breadcrumb(resp, modulo):
+    assert_contains(resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>')
