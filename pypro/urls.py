5bc7750dc5d8c1383ce2957e83886e0fc744b96b
kiko <cahenriquesoares@gmail.com>
2023-05-02 14:28:32 -0300

    Criada página de login
    Close #113

--- pypro/urls.py
+++ pypro/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include('django.contrib.auth.urls')),
     path('', include('pypro.base.urls')),
     path('aperitivos/', include('pypro.aperitivos.urls')),
     path('modulos/', include('pypro.modulos.urls')),
